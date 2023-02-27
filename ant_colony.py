f1232c4ce743ffc8bd5cc8c65340e0a32735c62c
andrb <borowiak.and@gmail.com>
2023-02-27 12:59:55 +0100

    Added main loop

--- ant_colony.py
+++ ant_colony.py
@@ -1,5 +1,27 @@
+import sys
 import pygame
 
 
 class AntColony:
-    pass
+    """Main class"""
+
+    def __init__(self):
+        """Program initialization"""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Ant Colony")
+
+    def run_program(self):
+        """Run main loop"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill((230, 230, 230))
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ac = AntColony()
+    ac.run_program()
