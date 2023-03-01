9ac44baa3290800935859946eeaa64d673ccef68
andrb <borowiak.and@gmail.com>
2023-03-01 08:39:22 +0100

    Chanhe in move_to_next_city method

--- ant.py
+++ ant.py
@@ -12,27 +12,24 @@ class Ant(Sprite):
         self.settings = ac_prog.settings
         self.city_to_visit = ac_prog.cities.copy()
         self.roads = ac_prog.roads
-        self.current_position = ac_prog.stats.start_city
+        self.start_city = ac_prog.stats.start_city
+        self.current_position = self.start_city
         self.visited_cities = pygame.sprite.Group()
         self.distance_traveled = 0
         self.journey_compleat = False
 
     def go_to_next_city(self):
-        """Move to next city"""
-        if len(self.city_to_visit) > 1:
-            self.visited_cities.add(self.current_position)
-            self.city_to_visit.remove(self.current_position)
+        """Move to next city as long as all cities will be visited. After that move to start city"""
+        self.visited_cities.add(self.current_position)
+        self.city_to_visit.remove(self.current_position)
+        if len(self.city_to_visit) > 0:
             self.current_position = self._choose_random_city()
-            last_road = self._find_last_road()
-            last_road.change_color()
-            self.distance_traveled += last_road.road_length
         else:
-            # TODO have to change to - move to last city and move to start. After that loop will be complete
-            # Move to start city
-            self.visited_cities.add(self.current_position)
-            self.current_position = self.city_to_visit.sprites()[0]
-            self.visited_cities.add(self.current_position)
+            self.current_position = self.start_city
             self.journey_compleat = True
+        last_road = self._find_last_road()
+        last_road.change_color()
+        self.distance_traveled += last_road.road_length
 
     def _choose_random_city(self):
         """Draw random city from not visited"""
